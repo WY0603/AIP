b62a2952dd18622da047d502b97bf732194687c4
WBruce <4952282@qq.com>
2018-08-20 16:07:17 +1000

    fix input bug

--- assignment3/src/Reserve.js
+++ assignment3/src/Reserve.js
@@ -10,13 +10,13 @@ class Reserve extends Component{
         return (
             <div>
                 <ur>
-                    <li><span>First Name: </span><input/></li>
-                    <li><span>Last Name: </span><input/></li>
-                    <li><span>Phone No.: </span><input/></li>
-                    <li><span>Email: </span><input/></li>
-                    <li><span>Date: </span><input/></li>
-                    <li><span>Time: </span><input/></li>
-                    <li><span>Number of Person: </span><input/></li>
+                    <li><span>First Name: </span><input type='text' name='firstname'/></li>
+                    <li><span>Last Name: </span><input  type='text' name='lastname'/></li>
+                    <li><span>Phone No.: </span><input  type='text' name='phonenumber'/></li>
+                    <li><span>Email: </span><input type='text' name='email'/></li>
+                    <li><span>Date: </span><input type='text' name='date'/></li>
+                    <li><span>Time: </span><input type='text' name='time'/></li>
+                    <li><span>Number of Person: </span><input type='text' name='total'/></li>
                 </ur>
                 <button>提交</button>
             </div>
@@ -28,7 +28,7 @@ class Reserve extends Component{
     }
 
     verify() {
-        
+
     }
 }
 
