f05be9842777cf1577b8fc1f43e9e20cd6801f6e
vickylin11 <37863598+vickylin11@users.noreply.github.com>
2018-08-27 20:25:29 +1000

    add home/res/resdetails component
    add home/res/resdetails component

---
+++ client/src/Resdetails.js
@@ -0,0 +1,21 @@
+Resdetails
+import React, { Component } from 'react';
+import './App.css';
+
+class Resdetails extends Component {
+  render() {
+
+    return (
+      <div>
+      <h1> resname </h1>
+      <h2>respic </h2>
+      <h2> resaddress </h2>
+      <h2> rescontactno</h2>
+    
+      
+      </div>
+    );
+  }
+}
+
+export default Resdetails;
