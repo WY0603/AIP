8253d681a5e7626f9fc04c2848516436572743c1
Veryinheart <danieljing1993@gmail.com>
2018-08-31 18:07:06 +1000

    update login router signup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink,
+    Switch
+} from 'react-router-dom'
+import Home from "./Home";
+import Signup from "./Signup";
+import Login from "./Login";
+import Restaurant from "./Restaurant";
+
+
+const App = () => (
+    <Router>
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to='/restaurant'>Restaurant</NavLink></li>
+
+                        <li><NavLink to='/login'>Login</NavLink></li>
+                        <li><NavLink to="/signup">Signup</NavLink></li>
+
+
+                    </ul>
+                </nav>
+            </header>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/restaurant" component={Restaurant}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/signup" component={Signup}/>
+
+            </Switch>
+        </div>
+    </Router>
+)
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- server.js
+++ server.js
@@ -1,11 +1,71 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+var md5 = require('blueimp-md5')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// http://mlab.com, account:wangyao0603, password:obsidian0822.
+var dbUrl = 'mongodb://aip:aip2018@ds241012.mlab.com:41012/aip';
+
+var User = mongoose.model('User', {
+    username: String,
+    password: String,
+    email: String
+
+})
+
+mongoose.connect(dbUrl, {useNewUrlParser : true}, (err) => {
+    console.log("mongo db connection", err);
+})
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.post('/register', (req, res) => {
+    var user = new User(req.body)
+
+    user.password = md5(md5(user.password))
+
+
+    user.save((err) => {
+        if(err)
+            sendStatus(500)
+    })
+
+    res.redirect('/restaurant')
+})
+
+app.post('/login',function (req,res) {
+
+    var user = new User(req.body);
+
+    User.findOne({
+            username: user.username,
+            password: md5(md5(user.password))
+        },function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                err_code: 500,
+                message: err.message
+            })
+        }
+        if (!user) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'Username or password is invalid.'
+            })
+        }
+        res.redirect('/restaurant')
+    }
+    )
+
+})
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
