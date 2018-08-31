6c0d0656c0df1cc91c54d3f8f6a382bef3846db9
Veryinheart <danieljing1993@gmail.com>
2018-08-31 15:21:54 +1000

    update react router

---
+++ dbtest.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
