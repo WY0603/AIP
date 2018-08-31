80deb6557d3241aa279be67375876bed64ba1f18
WY0603 <4952282@qq.com>
2018-08-31 16:01:00 +1000

    add register service

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 var dbUrl = 'mongodb://aip:aip2018@ds241012.mlab.com:41012/aip';
 
 var User = mongoose.model('User', {
-    userName: String,
+    username: String,
     password: String,
     email: String
 
@@ -18,6 +18,17 @@ mongoose.connect(dbUrl, {useNewUrlParser : true}, (err) => {
 })
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.post('/register', (req, res) => {
+  var user = new User(req.body)
+
+  user.save((err) => {
+    if(err)
+      sendStatus(500)
+  })
+
+  res.sendStatus(200)
+})
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
