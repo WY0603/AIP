c17db245b8fad58ff65e1aae5dacae129d91a015
vickylin11 <vickylzy11@gmail.com>
2018-09-24 14:36:36 +1000

    modified reservation & add veridation

--- client/src/Reservation.js
+++ client/src/Reservation.js
@@ -5,13 +5,13 @@ import Header from "./Header";
 export default class Reservation extends Component {
 constructor(props) {
         super(props);
-
+       
         this.state = {
             "resname": "",
             "r_id" : this.props.match.params.rid,
             "email": "",
             "time": "10:00",
-            "date":"",
+            "date": setDate(),
             "cusno": "1",
         };
 
@@ -30,6 +30,14 @@ constructor(props) {
     //     })
 
 handleSubmit(){
+var emailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+if (this.state.email.trim().length == 0 &&
+    this.state.date !== null){
+  alert ("* area must be filled.")
+}else if (!emailReg.test(this.state.email)) {
+            alert ("Pleaze enter a valid email address.");
+            return false;
+        }else{
 fetch('/reservation',{
             method:'post',
             body: JSON.stringify(this.state),
@@ -48,6 +56,7 @@ fetch('/reservation',{
 
         })
 }
+}
 
 
 render(){
@@ -59,9 +68,9 @@ render(){
       <h1 style={{marginLeft:150}}> {this.setState.resname} </h1>
       <Form style={{marginLeft:150}}>
         <FormGroup>
-          <Label for="exampleSelect">Number of Customers</Label>
+          <Label for="exampleSelect">Number of Customers: <span style={{color:"red"}}>* </span> </Label> 
           <Input type="select" name="select" id="exampleSelect" style={{height:20,width:100}}
-            onChange={evt => this.setState({"cusno":evt.target.value})}>
+            onChange={evt => this.setState({"cusno":evt.target.value})}> 
              
             <option>1</option>
             <option>2</option>
@@ -74,10 +83,11 @@ render(){
             <option>9</option>
             <option>10</option>
           </Input>
+          <p style={{color:'red'}}>* For group customer more than 10 people, please contact the restaurant for arrangement.  </p>
         </FormGroup>
 
         <FormGroup>
-          <Label for="exampleSelect">Time</Label>
+          <Label for="exampleSelect">Time: <span style={{color:"red"}}>* </span></Label>
           <Input type="select" name="select" id="exampleSelect" style={{height:20,width:100}}
             onChange={evt => this.setState({"time":evt.target.value})}>
               
@@ -96,13 +106,13 @@ render(){
           </Input>
         </FormGroup>
         <FormGroup>
-          <Label for="exampleDate">Date</Label>
+          <Label for="exampleDate">Date: <span style={{color:"red"}}>* </span></Label>
           <Input type="date" name="date" id="exampleDate" placeholder="date placeholder" style={{height:30,width:200}}
-              onChange={evt => this.setState({"date":evt.target.value})}/>  
+              value={this.state.date} onChange={evt => this.setState({"date":evt.target.value})}/>  
         </FormGroup>
 
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
+          <Label for="exampleEmail">Email:<span style={{color:"red"}}>* </span></Label>
           <Input type="email" name="email" id="exampleEmail" placeholder="Email" style={{height:30,width:200}}
            value={this.state.email} onChange={evt => this.setState({"email":evt.target.value})}/>   
           <p style={{color:'red'}}>* Confirmation message will be sent to this email address. </p>
@@ -117,3 +127,37 @@ render(){
 }
 
 }
+
+
+function setDate(){
+
+var date = new Date();
+
+// get month
+var nowMonth = date.getMonth() + 1;
+
+// get date
+var strDate = date.getDate();
+
+// add sperator
+var seperator = "-";
+
+// handle month 
+if (nowMonth >= 1 && nowMonth <= 9) {
+   nowMonth = "0" + nowMonth;
+}
+
+// handle date
+if (strDate >= 0 && strDate <= 9) {
+   strDate = "0" + strDate;
+}
+
+// fomulate date format
+var nowDate = date.getFullYear() + seperator + nowMonth + seperator + strDate;
+return nowDate;
+
+}
+
+
+
+
